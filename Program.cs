4386e6ef6c013a5cc3d63aafa18d5b8bf259ea5d
Laura <Laura@DESKTOP-BESSB6E>
2023-08-24 00:02:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Prasau iveskite slaptazodi: ");
+string enterPassword = Console.ReadLine();
+string password = "Laura";
+if (enterPassword == password)
+{
+    Console.WriteLine("Teisingai prisijungete");
+}
+else if (enterPassword == "Mellon")
+{
+    Console.WriteLine("Teisingai prisijungete");
+}
+else if (enterPassword == "01101001 01101110")
+{
+    Console.WriteLine("Nulauzta");
+}
+else
+{
+    Console.WriteLine("Slaptažodis neteisingas, prašome bandyti dar kartą");
+}
